b500dedfec31a5e8b0d8e879f528f216d325981b
MobiusM <danholandi@gmail.com>
2018-08-03 21:03:23 +0300

    static: js: time: always display time in 24h format

--- static/js/time.js
+++ static/js/time.js
@@ -2,8 +2,10 @@ var interval = setInterval(timestamp, 1000);
 
 
  function timestamp(){
+ var dateOptions = { hour12: false };
+ var dateLocale = 'en-US'
  var date = new Date();
 
  var timeElement = document.getElementById('clock');
- timeElement.innerHTML = date.toLocaleTimeString();
+ timeElement.innerHTML = date.toLocaleTimeString(dateLocale, dateOptions);
   }
